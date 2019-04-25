748ed0ab1e482d8d560670b18f742d78eb154030
aromerovilla <44728891+aromerovilla@users.noreply.github.com>
2019-04-25 12:38:07 +0200

    commit for aromerovilla 2.0

---
+++ tests/test_median.py
@@ -0,0 +1,14 @@
+from ie_pandas.DataFrame import DataFrame
+import pytest
+
+
+def test_median():
+    dict_x = {"name": ["Tom", "Harry", "Dick", "Jerry"], "age": [10, 20, 30, 40], "cars": [1, 2, 3, 4]}
+
+    df1 = DataFrame(dict_x)
+
+    expected_output = [{'age': 25, 'cars': 2.5}]
+
+    actual_output = df1.median()
+
+    assert actual_output == expected_output
---
+++ tests/test_min.py
@@ -0,0 +1,14 @@
+from ie_pandas.DataFrame import DataFrame
+import pytest
+
+
+def test_min():
+    dict_x = {"name": ["Tom", "Harry", "Dick", "Jerry"], "age": [10, 20, 30, 40], "cars": [1, 2, 3, 4]}
+
+    df1 = DataFrame(dict_x)
+
+    expected_output = [{'age': 10, 'cars': 1}]
+
+    actual_output = df1.min()
+
+    assert actual_output == expected_output
