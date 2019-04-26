fccb73e5cac986bc1a008d7f7fc10178fcfa3a58
aromerovilla <44728891+aromerovilla@users.noreply.github.com>
2019-04-26 16:51:37 +0200

    Creating test Head()

---
+++ tests/head.py
@@ -0,0 +1,41 @@
+#Creating a test for the Head() function
+
+def test_head(df):
+    
+    #This is the DataFrame that we are going to use
+    
+    dict_x = {"name": ["Tom", "Harry", "Dick", "Jerry"], "age": [10, 20, 30, 40], "salary":[50, 60, 70, 80], "adress":['a', 'b', 'c', 'd']}
+
+    df1 = DataFrame(dict_x)
+
+    l = []
+    
+    for i in df.column_names():
+        
+        a = i
+        
+        l.append(a)
+    
+
+    q = []
+    
+    for i in l:
+        
+        a = df[i][0]
+        
+        q.append(a)
+    
+    
+    expected_output1 = ['name', 'age', 'salary', 'adress']
+    
+    actual_output1 = l
+
+    expected_output2 = ['Tom', 10, 50, 'a']
+    
+    actual_output2 = q
+    
+    #With the Assert we are checking that everything is correct
+    
+    assert actual_output1 == expected_output1
+    
+    assert actual_output2 == expected_output2
