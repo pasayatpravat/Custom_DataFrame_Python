36f1af624495bb84cfe0e55a1c7475e45ebbdec9
Camille <44060754+CamilleCHAU@users.noreply.github.com>
2019-05-05 18:53:27 +0200

    Update test-get-row

--- tests/test_get_row.py
+++ tests/test_get_row.py
@@ -4,15 +4,50 @@ import pytest
 
 
 dict_x = {"name": ["Tom", "Harry", "Dick", "Jerry"], "age": [10, 20, 30, 40]}
-
-
 df1 = DataFrame(dict_x)
 
 
-def test_get_row():
+def test_get_row_scenario_1():
 
-    expected_output = [{'name': 'Tom', 'age': 10}]
+    expected_output = [{"name": "Tom", "age": 10}, {"name": "Harry", "age": 20}]
 
-    actual_output = df1.get_rows(0, 1)
+    actual_output = df1.get_rows(0, 2)
 
-    assert (actual_output == expected_output)
+    assert actual_output == expected_output
+
+
+def test_get_row_scenario_2():
+
+    expected_output = [{"name": "Tom", "age": 10}]
+
+    actual_output = df1.get_rows(0)
+
+    assert actual_output == expected_output
+
+
+def test_get_row_error_scenario_1():
+
+    with pytest.raises(ValueError) as excinfo:
+        df1.get_rows("0", "1")
+    assert str(excinfo.value) == "Wrong_Input_Type: Only integer value is acceptable"
+
+
+def test_get_row_error_scenario_2():
+
+    with pytest.raises(ValueError) as excinfo:
+        df1.get_rows(0, "1")
+    assert str(excinfo.value) == "Wrong_Input_Type: Only integer value is acceptable"
+
+
+def test_get_row_error_scenario_3():
+
+    with pytest.raises(ValueError) as excinfo:
+        df1.get_rows("0", 1)
+    assert str(excinfo.value) == "Wrong_Input_Type: Only integer value is acceptable"
+
+
+def test_get_row_error_scenario_4():
+
+    with pytest.raises(ValueError) as excinfo:
+        df1.get_rows("0")
+    assert str(excinfo.value) == "Wrong_Input_Type: Only integer value is acceptable
