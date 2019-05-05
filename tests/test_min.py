1ac099d3842d3e8ec0f8a0b27589d5632efbaa6a
alombatti <43571061+alombatti@users.noreply.github.com>
2019-05-05 19:10:40 +0200

    Update test min

--- tests/test_min.py
+++ tests/test_min.py
@@ -4,11 +4,13 @@ import pytest
 
 def test_min():
     dict_x = {"name": ["Tom", "Harry", "Dick", "Jerry"],
-              "age": [10, 20, 30, 40], "cars": [1, 2, 3, 4]}
+              "age": [10, 20, 30, 40],
+              "cars": [1, 2, 3, 4]
+             }
 
     df1 = DataFrame(dict_x)
 
-    expected_output = [{'age': 10, 'cars': 1}]
+    expected_output = [{“age”: 10, “cars”: 1}]
 
     actual_output = df1.min()
 
